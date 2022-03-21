f22d9548535175ebaccace045920bda651fe67d8
quangr <quangr@163.com>
2022-03-21 03:34:58 +0000

    start training

--- envpool/entry.py
+++ envpool/entry.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 """Entry point for all envs' registration."""
 
-import envpool.atari.registration  # noqa: F401
+# import envpool.atari.registration  # noqa: F401
 import envpool.classic_control.registration  # noqa: F401
-import envpool.toy_text.registration  # noqa: F401
-import envpool.vizdoom.registration  # noqa: F401
+# import envpool.toy_text.registration  # noqa: F401
+# import envpool.vizdoom.registration  # noqa: F401
